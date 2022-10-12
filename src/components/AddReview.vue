587e281a288cf3bbac8fbf1ac1dade79c0130ea4
sugyo <sg.guru1030@gmail.com>
2022-10-13 00:32:44 +1000

    commit-6

---
+++ src/components/AddReview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="hello">
+      <h1>{{ msg }}</h1>
+        Home page
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'home-page',
+    props: {
+      msg: String
+    }
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  </style>
+  
